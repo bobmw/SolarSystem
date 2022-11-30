56eaa9da2e26eb7c656bc783c9b9a62d9d29a6e5
bobFCamara <71573465+bob-mw@users.noreply.github.com>
2022-11-30 00:14:22 -0300

    refactor(Scene): setted up sunRef to put godrays

--- src/containers/Scene.js
+++ src/containers/Scene.js
@@ -4,8 +4,12 @@ import { OrbitControls } from "@react-three/drei";
 import { planetsConfig } from "../config/planets.config";
 import { MOUSE } from "three";
 import StarDome from "../components/StarDome";
+import { useState } from "react";
+import PostProcessing from "../components/PostProcessing";
 
 export default function Scene() {
+  const [sunRef, setSunRef] = useState(null);
+
   const mouseConfig = {
     LEFT: MOUSE.ROTATE,
     MIDDLE: MOUSE.DOLLY
@@ -14,11 +18,12 @@ export default function Scene() {
   return (
     <>
       <OrbitControls minDistance={30} maxDistance={200} mouseButtons={mouseConfig} />
-      <ambientLight intensity={0.8} />
-      <pointLight intensity={5}/>
+      <ambientLight intensity={0.5} />
+      <pointLight intensity={2} />
       <StarDome />
       <group>
-        <Sun />
+        <PostProcessing sun={sunRef} />
+        <Sun ref={setSunRef} />
         {planetsConfig.map((props, index) => (
           <Planet {...props} key={index} />
         ))
